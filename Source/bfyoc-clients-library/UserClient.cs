0c9f138ab590b8e030f27844fb6cdcf03b7c1512
Kevin Barrett <kebar@microsoft.com>
2020-09-15 17:59:53 -0600

    added objects project, client project and wired up objects and clients to users and products API

---
+++ Source/bfyoc-clients-library/UserClient.cs
@@ -0,0 +1,24 @@
+﻿using bfyoc_objects_library;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bfyoc_clients_library
+{
+    public class UserClient
+    {
+        private string userUrl = "https://serverlessohuser.trafficmanager.net/api/GetUser";
+        private static HttpClient httpClient = new HttpClient();
+
+        public async Task<User> GetUserAsync(Guid userId)
+        {
+            var finalProductUrl = string.Format("{0}?userId={1}", userUrl, userId.ToString());
+            var responseText = await httpClient.GetStringAsync(finalProductUrl);
+            var user = JsonConvert.DeserializeObject<User>(responseText);
+            return user;
+        }
+    }
+}
--- Source/bfyoc-rating-api/Function1.cs
+++ Source/bfyoc-rating-api/Function1.cs
@@ -7,28 +7,42 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using bfyoc_clients_library;
 
 namespace bfyoc_rating_api
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
+        [FunctionName("CreateRating")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            string userId = data?.userId;
+            string productId = data?.productId;
+            string locationName = data?.locationName;
+            string rating = data?.rating;
+            string userNotes = data?.userNotes;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
+            string responseMessage = 
+            string.IsNullOrEmpty(userId) || 
+            string.IsNullOrEmpty(productId) || 
+            string.IsNullOrEmpty(locationName) ||  
+            string.IsNullOrEmpty(rating) ||
+            string.IsNullOrEmpty(userNotes)
+                ? "This HTTP triggered function executed successfully. Pass UserId, ProductID, LocationName, Rating and UserNotes in the request body for a personalized response."
+                : $"{userId} {productId} {locationName} {rating} {userNotes}. This HTTP triggered function executed successfully.";
+
+            var productClient = new ProductClient();
+            var userClient = new UserClient();
+            var products = await productClient.GetProductsAsync();
+            var product = await productClient.GetProductAsync(new Guid(productId));
+            var user = await userClient.GetUserAsync(new Guid(userId));
             return new OkObjectResult(responseMessage);
         }
     }
