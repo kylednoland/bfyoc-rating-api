c4bdcffb8e04a8f99ee2b65e474f01752607023c
Scott Rutz <scrutz@microsoft.com>
2020-09-17 08:20:53 -0500

    checkpoint

---
+++ Source/bfyoc-rating-api/BatchTrackerIncrementEndpoint.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+
+namespace bfyoc_rating_api
+{
+    public static class BatchTrackerIncrementEndpoint
+    {
+        [FunctionName("BatchTrackerIncrementEndpoint")]
+        public static async Task<IActionResult> HttpStart([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Tracker/{batchId}")] HttpRequest req,
+            [DurableClient] IDurableClient context,
+            string batchId)
+        {
+            var entityId = new EntityId("BatchTracker", batchId);
+            await context.SignalEntityAsync(entityId, "Add");
+            var currentValue = await context.ReadEntityStateAsync<BatchTracker>(entityId);
+            return new OkObjectResult(currentValue);
+        }
+    }
+}
---
+++ Source/bfyoc-rating-api/BlobEventProcessor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bfyoc_rating_api
+{
+    public static class BlobEventProcessor
+    {
+        [FunctionName("BlobEventProcessor")]
+        public static async Task Run([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
+        {
+            log.LogInformation(eventGridEvent.Data.ToString());
+        }
+    }
+}
